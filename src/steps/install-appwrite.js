f1ce0e02479634632f9e0fad6527e13e99058cdf
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-05-31 13:26:48 -0400

    test: Against beta version

--- src/steps/install-appwrite.js
+++ src/steps/install-appwrite.js
@@ -3,10 +3,9 @@ import * as core from '@actions/core';
 import ExecOutput from '../helpers/ExecOutput.js';
 import RunCommand from '../helpers/RunCommand.js';
 
-
 export async function installAppwrite() {
   core.info('Installing Appwrite');
-  const res = await RunCommand.run('npm', ['install', '-g', 'appwrite']);
+  const res = await RunCommand.run('npm', ['install', '-g', 'appwrite-cli-beta@0.16.0b35b4bb46a448d7115c16b43031a1ddd2fa28982']);
 
   return res.exitCode;
 }
